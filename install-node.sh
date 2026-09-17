da89574e406e4f0fb3642489e86880c76e0d8bce
darktunnelmika <mikakhadmi@gmail.com>
2026-09-18 01:52:59 +0330

    release: bump DARK NOC to v2.9.48

--- install-node.sh
+++ install-node.sh
@@ -8,7 +8,7 @@ fi
 
 echo ""
 echo "  DARK NOC // NODE PREREQUISITES"
-echo "  Zero-touch Node v2.9.47"
+echo "  Zero-touch Node v2.9.48"
 echo ""
 export DEBIAN_FRONTEND=noninteractive
 apt-get update -qq
